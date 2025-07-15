9bafe7c42c29ae6c00dbeca11f0ae1c7805204ea
Kaylyn Yuh <yuhkay4@gmail.com>
2025-07-15 12:04:25 -0700

    componentizing, wip

---
+++ src/components/LeftPanel/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const LeftPanel = () => {
+  return (
+    <aside>
+      <h1>Blocks</h1>
+    </aside>
+  );
+};
+
+export default LeftPanel;
